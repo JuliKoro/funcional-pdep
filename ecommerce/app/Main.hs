52813e899474ac34a2290de8fa12a11b119aa84a
Julián Koroluk <julian.koroluk@outlook.com>
2022-04-28 19:53:41 -0300

    Lib.hs y Main.hs modificados

--- ecommerce/app/Main.hs
+++ ecommerce/app/Main.hs
@@ -1,47 +1,6 @@
-take :: Ord a => Int -> [a] -> [a]
+module Main where
 
-drop :: Ord a => Int -> [a] -> [a]
+import Lib
 
-head :: [a] -> a
-
-elem :: Eq a => a -> [a] -> Bool
-
-reverse :: [a] -> [a]
-
-productoDeLujo :: Num a => (String a) -> Bool
-productoDeLujo producto = (elem 'x' producto) || (elem 'z' producto)
-
-productoCodiciado :: String -> Bool
-productoCodiciado producto = (length producto) >= 10
-
-productoCorriente :: String -> Bool
-productoCorriente producto = head ['a', 'e', 'i', 'o', 'u'] producto
-
-productoDeElite :: String -> Bool
-productoDeElite producto = productoDeLujo producto && productoCodiciado producto && not productoCorriente
-
-descodiciarProducto :: String -> String
-descodiciarProducto producto = take ((length producto) - 10) producto
-
-productoXL :: String -> String
-productoXL producto = producto ++ "XL"
-
-versionBarata :: String -> String
-versionBarata producto = reverse . descodiciarProducto producto $ producto
-
-precioSubTotal :: Num a => precioUnitario * cantidad
-precioSubTotal precioUnitario cantidad = precioUnitario * cantidad
-
-aplicarDescuento :: Num a => a -> a -> a
-aplicarDescuento precioUnitario descuento = 
-    precioSubTotal $ precioUnitario - precioSubTotal $ precioUnitario * (descuento / 100)
-
-aplicarCostoDeEnvio :: Num a => a -> a -> a
-aplicarCostoDeEnvio precioUnitario costoDeEnvio = aplicarDescuento (precioUnitario) + costoDeEnvio
-
-precioTotal :: Num a => a -> Int -> a -> a -> a 
-precioTotal precioUnitario cantidad descuento costoDeEnvio = 
-    aplicarCostoDeEnvio . (aplicarDescuento . precioSubTotal) $ precioUnitario
-
-entregaSencilla :: String -> Bool
-entregaSencilla diaDeEntrega = even . length $ diaDeEntrega 
+main :: IO ()
+main = someFunc
--- ecommerce/src/Lib.hs
+++ ecommerce/src/Lib.hs
@@ -1,6 +1,44 @@
-module Lib
-    ( someFunc
-    ) where
+{-
+take :: Int -> [a] -> [a]
+drop :: Int -> [a] -> [a]
+head :: [a] -> a
+elem :: Eq a => a -> [a] -> Bool
+reverse :: [a] -> [a]
+-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+precioTotal :: Float -> Float -> Float -> Float -> Float
+precioTotal precioUnitario cantidad descuento costoDeEnvio = aplicarCostoDeEnvio (aplicarDescuento precioUnitario descuento * cantidad) costoDeEnvio
+
+productoDeElite :: String -> Bool
+productoDeElite nombreDeProducto = productoDeLujo nombreDeProducto && productoCodiciado nombreDeProducto && (not . productoCorriente) nombreDeProducto
+
+aplicarDescuento :: Float -> Float -> Float
+aplicarDescuento unPrecio unDescuento = unPrecio * (1 - unDescuento)
+
+entregaSencilla :: String -> Bool
+entregaSencilla unDia = even . length $ unDia
+
+descodiciarProducto :: String -> String
+descodiciarProducto nombreDeProducto = take 10 nombreDeProducto
+
+productoDeLujo :: String -> Bool
+productoDeLujo nombreDeProducto = elem 'x' nombreDeProducto || elem 'z' nombreDeProducto
+
+aplicarCostoDeEnvio :: Float -> Float -> Float
+aplicarCostoDeEnvio unPrecio unCostoDeEnvio = unPrecio + unCostoDeEnvio
+
+productoCodiciado :: String -> Bool
+productoCodiciado nombreDeProducto = length nombreDeProducto > 10
+
+productoCorriente :: String -> Bool
+productoCorriente nombreDeProducto = esVocal . head $ nombreDeProducto
+
+esVocal :: Char -> Bool
+esVocal unaLetra = elem unaLetra "aeiouAEIOU"
+
+productoXL :: String -> String
+productoXL nombreDeProducto = nombreDeProducto ++ " XL"
+
+versionBarata :: String -> String
+versionBarata nombreDeProducto = reverse . descodiciarProducto $ nombreDeProducto
