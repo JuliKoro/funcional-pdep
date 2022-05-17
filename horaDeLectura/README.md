3bf0f7970540e55ecca4491c1a8f0aeef61aca13
Julián Koroluk <julian.koroluk@outlook.com>
2022-05-17 16:23:17 -0300

    Hecho en Clase 4 y README.md

--- horaDeLectura/README.md
+++ horaDeLectura/README.md
@@ -1,1 +1,23 @@
-# horaDeLectura
+# ¡Hora de Lectura!
+
+_Tarea del 10/04_
+
+![Hora de Lectura](/horaDeLectura/horaDeLectura.png)
+
+Leer es una actividad que solemos dejar de lado por el tiempo que lleva hacerlo. Pero es un lindo hábito que está bueno mantener, ¿no? 📖
+En PdeP tenemos una biblioteca con nuestros libros favoritos; te contamos algunos 📚. El visitante de Stephen King que tiene 592 páginas, Shingeki no Kyojin capítulo 1, 3 y 127 que tienen 40 páginas cada uno y los escribió Hajime Isayama, Fundación de Isaac Asimov con 230 páginas, los capítulos  5, 10 y 12 de Sandman de Neil Gaiman con 35 páginas cada uno y, por último, la saga de Eragon: Eragon (544 páginas), Eldest (704 páginas), Brisignr (700 páginas) y Legado (811 páginas) de Christopher Paolini.
+
+¡Un montón! Nos gustaría organizarlos un poco 😅. Estas son cosas que nos gustarían saber:
+
+- El **promedioDePaginas** de nuestra biblioteca.
+- Qué lectura es una **lecturaObligatoria**, esto es así cuando es de Stephen King o de la saga de Eragon o es el ejemplar de Fundación de 230 páginas de Isaac Asimov (¡ningún otro!).
+- Si la biblioteca es **fantasiosa**, es decir, si tiene algún libro de Christopher Paolini o de Neil Gaiman.
+- El **nombreDeLaBiblioteca**, que es el nombre de todos los títulos juntos, sacándole las vocales.
+- Si tenemos una **bibliotecaLigera**, o sea, si todas sus lecturas tienen 40 páginas o menos.
+- El **género** de un libro. Diremos que:
+  * todos los libros escritos por Stephen King son de terror 😱
+  * los libros escritos por un autor Japonés 🎌 son manga;
+  * los que tienen menos de 40 páginas son cómics 🦸🏼
+  * el resto no están clasificados.
+
+Definí las funciones y valores necesarios para poder consultar lo listado anteriormente. Y sí, todas las funciones que crees deberán estar tipadas. ¡Manos al teclado! 👩‍💻👨‍💻 
--- horaDeLectura/src/Lib.hs
+++ horaDeLectura/src/Lib.hs
@@ -1,6 +1,103 @@
-module Lib
-    ( someFunc
-    ) where
+type Titulo = String -- alias de tipos
+type Autor = String 
+type Paginas = Int
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type Libro = (Titulo, Autor, Paginas) 
+
+elVisitante :: Libro -- es lo mismo que hacer "elVisitante :: (String, String, Int)"
+elVisitante = ("Stephen King", "El Visitante", 592) -- definicion de etiquetas
+
+shingeki1 :: Libro
+shingeki1   = ("Hajime Isayama", "Shingeki no Kyojin 1",   40)
+
+shingeki3 :: Libro
+shingeki3   = ("Hajime Isayama", "Shingeki no Kyojin 3",   40)
+
+shingeki127 :: Libro
+shingeki127 = ("Hajime Isayama", "Shingeki no Kyojin 127", 40)
+
+fundacion :: Libro
+fundacion = ("Isaac Asimov", "Fundación", 230)
+
+sandman5 :: Libro
+sandman5 = ("Neil Gaiman", "Sandman 5", 35)
+
+sandman10 :: Libro
+sandman10 = ("Neil Gaiman", "Sandman 10", 35)
+
+sandman12 :: Libro
+sandman12 = ("Neil Gaiman", "Sandman 12", 35)
+
+eragon :: Libro
+eragon = ("Christopher Paolini", "Eragon", 544)
+
+eldest :: Libro
+eldest = ("Christopher Paolini", "Eldest", 704)
+
+brisignr :: Libro
+brisignr = ("Christopher Paolini", "Brisignr", 700)
+
+legado :: Libro
+legado = ("Christopher Paolini", "Legado", 811)
+
+type Biblioteca = [Libro] -- Lista de Libros
+
+miBiblioteca :: Biblioteca 
+miBiblioteca = [elVisitante, shingeki1, shingeki3, shingeki127, sandman5, sandman10, sandman12, legado, brisignr, eragon, eldest, fundacion]
+
+promedioDePaginas :: Biblioteca -> Int -- `div`para que sea infija
+promedioDePaginas unaBiblioteca = sumatoriaPaginasBiblioteca unaBiblioteca `div` length unaBiblioteca
+
+sumatoriaPaginasBiblioteca :: Biblioteca -> Int
+sumatoriaPaginasBiblioteca unaBiblioteca = sum . map paginas $ unaBiblioteca
+
+paginas :: Libro -> Int -- Accesor -> retorna solamente un elemento especifico de la terna
+paginas (_, _, paginas) = paginas
+
+lecturaObligatoria :: Libro -> Bool
+lecturaObligatoria unLibro = esDeAutor "Stephen King" unLibro || esDeSagaEragon unLibro || unLibro == fundacion
+
+esDeAutor :: String -> Libro -> Bool
+esDeAutor unAutor unLibro = autor unLibro == unAutor
+
+autor :: Libro -> String
+autor (unAutor, _, _) = unAutor
+
+esDeSagaEragon :: Libro -> Bool
+esDeSagaEragon unLibro = elem (titulo unLibro) ["Eragon", "Eldest", "Legado", "Brisignr"]
+
+titulo :: Libro -> String
+titulo (_, unTitulo, _) = unTitulo
+
+bibliotecaFantasiosa :: Biblioteca -> Bool
+bibliotecaFantasiosa unaBiblioteca = any esLibroFantasioso unaBiblioteca
+
+esLibroFantasioso :: Libro -> Bool
+esLibroFantasioso unLibro = esDeAutor "Christopher Paolini" unLibro || esDeAutor "Neil Gaiman" unLibro
+
+nombreDeLaBiblioteca :: Biblioteca -> String
+nombreDeLaBiblioteca unaBiblioteca = sinVocales . concat . listaDeTitulos $ unaBiblioteca
+
+sinVocales :: String -> String
+sinVocales unNombre = filter (not . esVocal) unNombre
+
+esVocal :: Char -> Bool -- Toma caracter por caracter de una lista (String)
+esVocal unCaracter = elem unCaracter "aeiouAEIOU"
+
+listaDeTitulos :: Biblioteca -> [String]
+listaDeTitulos unaBiblioteca = map titulo unaBiblioteca
+
+bibliotecaLigera :: Biblioteca -> Bool
+bibliotecaLigera unaBiblioteca = all esLecturaLigera unaBiblioteca
+
+esLecturaLigera :: Libro -> Bool
+esLecturaLigera unLibro = paginas unLibro <= 40
+
+genero :: Libro -> String -- Guardas: Retorna en el tipo de la funcion (¡menos Bool!) en distintos casos
+genero unLibro
+  | esDeAutor "Stephen King" unLibro = "Terror"
+  | esDeAutorJapones unLibro = "Manga"
+  | paginas unLibro < 40 = "Comic"
+
+esDeAutorJapones :: Libro -> Bool
+esDeAutorJapones unLibro = esDeAutor "Hajime Isayama" unLibro
