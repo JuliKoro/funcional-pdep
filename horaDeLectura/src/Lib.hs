9fb927d73fc8a84d5e8222176f5ced1bd23985fd
Julián Koroluk <julian.koroluk@outlook.com>
2022-05-20 18:07:19 -0300

    Refactor con pattern matching, data y record syntax

--- horaDeLectura/src/Lib.hs
+++ horaDeLectura/src/Lib.hs
@@ -2,43 +2,50 @@ type Titulo = String -- alias de tipos
 type Autor = String 
 type Paginas = Int
 
-type Libro = (Titulo, Autor, Paginas) 
+data Libro = UnLibro { -- Data (crear un tipo de dato)
+  autor :: String, -- record sintax (reemplaza los accesors)
+  titulo :: String,
+  paginas :: Int
+} deriving (Show, Eq) -- le doy pertenencia a los Type Class que quiero
 
-elVisitante :: Libro -- es lo mismo que hacer "elVisitante :: (String, String, Int)"
-elVisitante = ("Stephen King", "El Visitante", 592) -- definicion de etiquetas
+-- elVisitante :: Libro -- es lo mismo que hacer "elVisitante :: (String, String, Int)"
+-- elVisitante = ("Stephen King", "El Visitante", 592) -- definicion de etiquetas
+
+elVisitante :: Libro 
+elVisitante = UnLibro "Stephen King" "El Visitante" 592
 
 shingeki1 :: Libro
-shingeki1   = ("Hajime Isayama", "Shingeki no Kyojin 1",   40)
+shingeki1 = UnLibro "Hajime Isayama" "Shingeki no Kyojin 1" 40
 
 shingeki3 :: Libro
-shingeki3   = ("Hajime Isayama", "Shingeki no Kyojin 3",   40)
+shingeki3 = UnLibro "Hajime Isayama" "Shingeki no Kyojin 3" 40
 
 shingeki127 :: Libro
-shingeki127 = ("Hajime Isayama", "Shingeki no Kyojin 127", 40)
+shingeki127 = UnLibro "Hajime Isayama" "Shingeki no Kyojin 127" 40
 
 fundacion :: Libro
-fundacion = ("Isaac Asimov", "Fundación", 230)
+fundacion = UnLibro "Isaac Asimov" "Fundación" 230
 
 sandman5 :: Libro
-sandman5 = ("Neil Gaiman", "Sandman 5", 35)
+sandman5 = UnLibro "Neil Gaiman" "Sandman 5" 35
 
 sandman10 :: Libro
-sandman10 = ("Neil Gaiman", "Sandman 10", 35)
+sandman10 = UnLibro "Neil Gaiman" "Sandman 10" 35
 
 sandman12 :: Libro
-sandman12 = ("Neil Gaiman", "Sandman 12", 35)
+sandman12 = UnLibro "Neil Gaiman" "Sandman 12" 35
 
 eragon :: Libro
-eragon = ("Christopher Paolini", "Eragon", 544)
+eragon = UnLibro "Christopher Paolini" "Eragon" 544
 
 eldest :: Libro
-eldest = ("Christopher Paolini", "Eldest", 704)
+eldest = UnLibro "Christopher Paolini" "Eldest" 704
 
 brisignr :: Libro
-brisignr = ("Christopher Paolini", "Brisignr", 700)
+brisignr = UnLibro "Christopher Paolini" "Brisignr" 700
 
 legado :: Libro
-legado = ("Christopher Paolini", "Legado", 811)
+legado = UnLibro "Christopher Paolini" "Legado" 811
 
 type Saga = [Libro]
 
@@ -56,25 +63,20 @@ promedioDePaginas unaBiblioteca = sumatoriaPaginasBiblioteca unaBiblioteca `div`
 sumatoriaPaginasBiblioteca :: Biblioteca -> Int
 sumatoriaPaginasBiblioteca unaBiblioteca = sum . map paginas $ unaBiblioteca
 
-paginas :: Libro -> Int -- Accesor -> retorna solamente un elemento especifico de la terna
-paginas (_, _, paginas) = paginas
+-- paginas :: Libro -> Int -- Accesor -> retorna solamente un elemento especifico de la terna
+-- paginas (_, _, paginas) = paginas
+-- al usar "record syntax" en los data no hace falta crear los accesors
 
 lecturaObligatoria :: Libro -> Bool -- Pattern Matching
-lecturaObligatoria ("Isaac Asimov", "Fundación", 230) = True -- del más específico
-lecturaObligatoria ("Stephen King", _, _) = True
+lecturaObligatoria (UnLibro "Isaac Asimov" "Fundación" 230) = True -- del más específico
+lecturaObligatoria (UnLibro "Stephen King" _ _) = True
 lecturaObligatoria unLibro = esDeSagaEragon unLibro -- al más genérico
 
 esDeAutor :: String -> Libro -> Bool
 esDeAutor unAutor unLibro = autor unLibro == unAutor
 
-autor :: Libro -> String
-autor (unAutor, _, _) = unAutor
-
 esDeSagaEragon :: Libro -> Bool
 esDeSagaEragon unLibro = elem unLibro sagaDeEragon
-
-titulo :: Libro -> String
-titulo (_, unTitulo, _) = unTitulo
 
 bibliotecaFantasiosa :: Biblioteca -> Bool
 bibliotecaFantasiosa unaBiblioteca = any esLibroFantasioso unaBiblioteca
@@ -108,3 +110,10 @@ genero unLibro
 
 esDeAutorJapones :: Libro -> Bool
 esDeAutorJapones unLibro = esDeAutor "Hajime Isayama" unLibro
+
+agregarPaginas :: Libro -> Int -> Libro -- retorna un nuevo libro distinto, no cambia el que le paso
+-- agregarPaginas (UnLibro unAutor unTitulo unasPaginas) paginasAAgregar = UnLibro unAutor unTitulo (unasPaginas + paginasAAgregar) -- libro abierto
+agregarPaginas unLibro unasPaginas = unLibro { paginas = paginas unLibro + unasPaginas } -- con record syntax
+
+sacarSecuela :: Libro -> Libro
+sacarSecuela unLibro = unLibro { titulo = titulo unLibro ++ " 2", paginas = paginas unLibro + 50 }
